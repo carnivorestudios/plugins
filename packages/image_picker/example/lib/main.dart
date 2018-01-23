66fbfc113a61c98d93666243e683dc73b02686ef
Philipp Comans <philipp.comans@gmail.com>
2018-01-22 18:05:03 -0800

    Buttons for image and video picking

--- packages/image_picker/example/lib/main.dart
+++ packages/image_picker/example/lib/main.dart
@@ -40,25 +40,37 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: new AppBar(
         title: const Text('Image Picker Example'),
       ),
-      body: new Center(
-          child: new FutureBuilder<File>(
-              future: _imageFile,
-              builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return new Image.file(snapshot.data);
-                } else {
-                  return const Text('You have not yet picked an image.');
-                }
-              })),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _imageFile = ImagePicker.pickImage();
-          });
-        },
-        tooltip: 'Pick Image',
-        child: new Icon(Icons.add_a_photo),
-      ),
+      body: new Column(children: <Widget>[
+        new FutureBuilder<File>(
+            future: _imageFile,
+            builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return new Image.file(snapshot.data);
+              } else {
+                return const Text('You have not yet picked an image.');
+              }
+            }),
+        new RaisedButton(
+          onPressed: () {
+            setState(() {
+              _imageFile = ImagePicker.pickImage(
+                type: ImageType.video,
+              );
+            });
+          },
+          child: new Text("Pick Video"),
+        ),
+        new RaisedButton(
+          onPressed: () {
+            setState(() {
+              _imageFile = ImagePicker.pickImage(
+                type: ImageType.picture,
+              );
+            });
+          },
+          child: new Text("Pick Image"),
+        ),
+      ]),
     );
   }
 }
